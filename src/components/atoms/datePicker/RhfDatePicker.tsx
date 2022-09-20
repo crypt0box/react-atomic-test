d201fd41fe3a5bb8f230122ab9ac71d02ba36797
cryptobox <pray,happy76@gmail.com>
2022-09-21 00:46:43 +0900

    feat: datepicker実装

--- src/components/atoms/datePicker/RhfDatePicker.tsx
+++ src/components/atoms/datePicker/RhfDatePicker.tsx
@@ -1,13 +1,15 @@
 import {
-  DeepMap,
-  FieldError,
   FieldValues,
   useController,
   UseControllerProps,
 } from "react-hook-form";
-import { DatePicker, DatePickerProps } from "@mui/x-date-pickers";
+import { DatePicker } from "@mui/x-date-pickers";
 import { RhfTextField } from "src/components/atoms/textFiled/RhfTextField";
 import { useState } from "react";
+import { parse } from "date-fns";
+
+/** 日付フォーマットyyyy/MM/ddを文字列とみなした時の長さは10 */
+const DATE_FORMAT_LENGTH = 10;
 
 export type RhfDatePickerProps<T extends FieldValues> = UseControllerProps<T>;
 
@@ -15,16 +17,35 @@ export const RhfDatePicker = <T extends FieldValues>(
   props: RhfDatePickerProps<T>
 ) => {
   const { name, control } = props;
-  const [dateValue, setDateValue] = useState<string>("");
+  const {
+    field: { onChange },
+  } = useController<T>({ name, control });
+  const [dateValue, setDateValue] = useState<Date | null>(null);
+
+  const onSelectDate = (e: Date | null) => {
+    setDateValue(e);
+    onChange(e);
+  };
+
+  const onChangeText = (value: string) => {
+    // MUIのDatePickerはデフォルトで10文字より多く入力できてしまうため、10文字を超えた分は省略する
+    // ex) yyyy/MM/dd{任意の文字}のように入力できてしまう
+    if (value.length > DATE_FORMAT_LENGTH) {
+      onChange(
+        parse(value.slice(0, DATE_FORMAT_LENGTH), "yyyy/MM/dd", new Date())
+      );
+      return;
+    }
+    onChange(parse(value, "yyyy/MM/dd", new Date()));
+  };
   return (
     <DatePicker
       value={dateValue}
-      onChange={(e) => setDateValue(e || "")}
-      inputFormat="yyyy年MM月dd日"
-      mask="____年__月__日"
+      onChange={(e: Date | null) => onSelectDate(e)}
       renderInput={(params) => (
         <RhfTextField
           {...params}
+          onChange={(e) => onChangeText(e.target.value)}
           defaultValue={undefined}
           name={name}
           control={control}
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,16 +3,17 @@ import { MuiButton } from "src/components/atoms/button/Button";
 import { Flex } from "src/components/atoms/flex/Flex";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { SubmitForm } from "src/components/molecules/submitForm/SubmitForm";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { RhfTextField } from "src/components/atoms/textFiled/RhfTextField";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { RhfDatePicker } from "src/components/atoms/datePicker/RhfDatePicker";
+import { DatePicker } from "@mui/x-date-pickers";
 
 const schema = z.object({
   comment: z.string().min(1, { message: "Required" }),
-  date: z.string(),
+  date: z.date().optional(),
 });
 
 type Form = z.infer<typeof schema>;
@@ -21,7 +22,7 @@ export const Home = () => {
   const { control, watch, handleSubmit } = useForm<Form>({
     defaultValues: {
       comment: "",
-      date: "",
+      date: undefined,
     },
     resolver: zodResolver(schema),
   });
